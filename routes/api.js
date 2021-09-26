61bbecd00e54776fe3db0b8c624cc65c4f7f708e
DevChaudhary78 <devchaudhary.dc84.dc@gmail.com>
2021-09-26 17:28:39 +0530

    added routes for create, update and delete post

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express.Router()
 const Post = require('../models/post')
 
+// get all posts
 app.get('/posts', async (req, res, next) => {
   try {
     const posts = await Post.find({})
@@ -13,5 +14,28 @@ app.get('/posts', async (req, res, next) => {
     next(err)
   }
 })
+
+// get single post
+app.get('/posts/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const post = await Post.findById(id)
+    if (!post) {
+      return res.status(404).json({ err: 'post by this id not found' })
+    }
+    res.status(200).json({ post })
+  } catch (err) {
+    next(err)
+  }
+})
+
+// create post
+app.post('/posts', (req, res, next) => {})
+
+// update post
+app.put('/posts/:id', (req, res, next) => {})
+
+// delete post
+app.delete('/posts/:id', (req, res, next) => {})
 
 module.exports = app
