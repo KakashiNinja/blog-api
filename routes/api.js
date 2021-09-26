6d9c36138e7257b251da9dee841e596f629d72ea
DevChaudhary78 <devchaudhary.dc84.dc@gmail.com>
2021-09-26 10:56:13 +0530

    added get route for all posts

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express.Router()
+const Post = require('../models/post')
 
-app.get('/posts', (req, res) => {
-  res.send()
+app.get('/posts', async (req, res, next) => {
+  try {
+    const posts = await Post.find({})
+    if (!posts) {
+      return res.status(404).json({ err: 'posts not found' })
+    }
+    res.status(200).json({ posts })
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = app
