ee4e5aeb4c214157c8d70c1e2a3f9ef516bf6b6f
DevChaudhary78 <devchaudhary.dc84.dc@gmail.com>
2021-09-27 10:32:17 +0530

    added comment routes

--- routes/api.js
+++ routes/api.js
@@ -2,19 +2,28 @@ const express = require('express')
 const app = express.Router()
 const post_controller = require('../controllers/postController')
 
-// get all posts
+// Post routes
 app.get('/posts', post_controller.get_posts)
 
-// get single post
 app.get('/posts/:id', post_controller.get_single_post)
 
-// create post
 app.post('/posts', post_controller.create_post)
 
-// update post
 app.put('/posts/:id', post_controller.update_post)
 
-// delete post
 app.delete('/posts/:id', post_controller.delete_post)
+
+// Comment routes
+app.post('/posts/:postid/comments')
+
+app.get('/posts/:postid/comments')
+
+app.get('/posts/:postid/comments/:commentid')
+
+app.put('/posts/:postid/comments/:commentid')
+
+app.delete('/posts/:postid/comments/:commentid')
+
+// Author routes
 
 module.exports = app
